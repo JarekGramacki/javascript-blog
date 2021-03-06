2ef5f2480d75ecc6f621c12428109b7cd25a5b19
Jarek Gramacki <gramacki.programowanie@gmail.com>
2021-03-06 15:18:20 +0100

    Add finish task with coments

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
   });*/
-
   
-  
+  // event to obiekt ktory opisuje zdarzenie, zawiera informacj gdzie byl kilk (poz X, Y),
+  // na jaki element (currentTarget=this=kliknieteElement) itp
   const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
@@ -13,8 +13,9 @@
     
     /*[DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
-
+    //w petli skaczemy po kazdym elemencie z listy activeLinks
     for(let activeLink of activeLinks){
+      // dla kazdego elementu usuwamy klase active z atrybutu class
       activeLink.classList.remove('active');
     }
   
@@ -23,20 +24,21 @@
     console.log('clickedElement:', clickedElement);
   
     /*[DONE] remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('posts');
-
+    const activeArticles = document.querySelectorAll('.post');
     for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
     }
   
     /* get 'href' attribute from the clicked link */
-    const articleSelector = element.getAttribute ('href');
-    console.log(articleSelector);
-  
+    let articleSelector = clickedElement.getAttribute('href');
+      
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    let postToActive = document.querySelector(articleSelector);
+   
     /* add class 'active' to the correct article */
+    postToActive.classList.add('active');
   }
+
   
   const links = document.querySelectorAll('.titles a');
   
