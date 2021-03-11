7a7833c8a10d9ce595e66e1b65414b34ac0038d2
Jarek Gramacki <gramacki.programowanie@gmail.com>
2021-03-11 23:13:09 +0100

    Generate html link

--- js/script.js
+++ js/script.js
@@ -101,10 +101,12 @@ function generateTags(){
     for (let tag of articleTagsArray){
     console.log('pojedyńcze tagi:', tag);
       /* generate HTML of the link */
-
+      const linkHTML = '<li><a href="#tag-' + tag + '">' + tag +'</a></li>';
+      console.log('link html:',linkHTML);
       /* add generated code to html variable */
 
     /* END LOOP: for each tag */
+ 
     }
     /* insert HTML of all the links into the tags wrapper */
 
