a7e5baf05c1c02da1c611b3e5db68a0153b8f36d
Jarek Gramacki <gramacki.programowanie@gmail.com>
2021-03-16 18:07:10 +0100

    Add link to allTags

--- js/script.js
+++ js/script.js
@@ -87,8 +87,8 @@ generateTitleLinks();
 
 
 function generateTags(){
-  /* [NEW] create a new variable allTags with an empty array */
-  let allTags = [];
+  /* [NEW] create a new variable allTags with an empty object */
+  let allTags = {};
 
   /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
@@ -109,9 +109,11 @@ function generateTags(){
       /* add generated code to html variable */
       html = html + linkHTML;
       /* [NEW] check if this link is NOT already in allTags */
-      if(allTags.indexOf(linkHTML) == -1){
-        /* [NEW] add generated code to allTags array */
-        allTags.push(linkHTML);
+      if(!allTags[tag]) {
+  /* [NEW] add tag to allTags object */
+      allTags[tag] = 1;
+      } else {
+        allTags[tag]++;
       }
     /* END LOOP: for each tag */
     }
@@ -122,9 +124,19 @@ function generateTags(){
    /* [NEW] find list of tags in right column */
    const tagList = document.querySelector('.tags');
 
-   /* [NEW] add html from allTags to tagList */
-   tagList.innerHTML = allTags.join(' ');
-}
+   /* [NEW] create variable for all links HTML code */
+    let allTagsHTML = '';
+
+  /* [NEW] START LOOP: for each tag in allTags: */
+    for(let tag in allTags){
+  /* [NEW] generate code of a link and add it to allTagsHTML */
+     allTagsHTML +='<li><a href="#tag-'+ tag  +' (' + allTags[tag] + ') '+'">'+ tag  +' (' + allTags[tag] + ') '+'</a></li>';
+    
+  /* [NEW] END LOOP: for each tag in allTags: */
+  }
+  /*[NEW] add HTML from allTagsHTML to tagList */
+    tagList.innerHTML = allTagsHTML;
+    }
 generateTags();
 
 
@@ -184,7 +196,7 @@ function generateAuthors(){
     let html = '';
     /* get autors from data-autor attribute */
     const articleAuthor = article.getAttribute('data-author');
-    console.log(articleAuthor);
+   
     /* generate HTML of the link */
     const linkHTML = '<li><a href="#author-' + articleAuthor + '">' +  articleAuthor  +'</a></li>';
     /* add generated code to html variable */
